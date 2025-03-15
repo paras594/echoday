29677ea7a1ed06b3ba9fd938d512e088c290ef0e
paras594 <parasarora594@gmail.com>
2025-03-16 02:18:31 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Typography } from "@progress/kendo-react-common";
+import "./App.css";
+import { StackLayout } from "@progress/kendo-react-layout";
+
+function App() {
+  return <h1>Hello world</h1>;
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from "./App.jsx";
+import "./index.scss";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
+
+/**
+<Routes>
+  <Route index element={<Home />} />
+  <Route path="about" element={<About />} />
+
+  <Route element={<AuthLayout />}>
+    <Route path="login" element={<Login />} />
+    <Route path="register" element={<Register />} />
+  </Route>
+
+  <Route path="concerts">
+    <Route index element={<ConcertsHome />} />
+    <Route path=":city" element={<City />} />
+    <Route path="trending" element={<Trending />} />
+  </Route>
+</Routes>
+*/
